cd78e06795930a9a2a7affa7faf4ac966fdf0676
Armin <Armin@DESKTOP-LVFB4U7>
2022-01-18 12:20:03 +0100

    Add project files.

---
+++ Fizz Buzz/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+    {
+
+        Console.WriteLine(i + " FIZZ BUZZ");
+    }
+    else if (i % 3 == 0)
+    {
+
+        Console.WriteLine(i +" FIZZ");
+    }
+    else if (i % 5 == 0)
+    {
+
+        Console.WriteLine(i + " BUZZ");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+};
